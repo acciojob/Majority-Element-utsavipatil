fe828e36706f448837bfcdecfc31c605ae743193
Utsavi Patil <abc@gmail.com>
2022-07-07 18:23:00 +0530

    S:O(1) T:O(n)

--- solution.js
+++ solution.js
@@ -1,6 +1,24 @@
 // arr = array of numbers
 function major(arr)
 {
-  //your code here
+    //S : O(1),  T : O(n)
+    let count = 1 , majIdx = 0;
+
+    for(let i=1; i<arr.length; i++){
+
+        if(arr[majIdx] == arr[i]){ //If same as Majority Element
+          count++;
+
+        }else{
+          count--;
+        }
+
+        if(count == 0){ //Reset values
+          count = 1;
+          majIdx = i;
+        }
+    }
+
+    return arr[majIdx];
 }
 module.exports=major;
